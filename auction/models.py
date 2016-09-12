83c4066062ee8817413671a51182d7599b8436c4
Paul Ortman <paul.ortman@gmail.com>
2016-09-11 21:18:39 -0500

    modify the models for null values

--- auction/models.py
+++ auction/models.py
@@ -7,7 +7,7 @@ class AuctionItem(models.Model):
 
     name = models.CharField(max_length=50, blank=False)
     long_desc = models.TextField(blank=True)
-    winning_bidder = models.ForeignKey('Bidder', blank=True)
+    winning_bidder = models.ForeignKey('Bidder', blank=True, null=True)
     winning_bid_time = models.DateTimeField(blank=True, null=True)
     auction_time = models.DateTimeField(blank=True, null=True)
 
@@ -15,15 +15,17 @@ class AuctionItem(models.Model):
         return reverse('auctionitem_detail', kwargs={'pk': self.pk})
 
     def save(self, *args, **kwargs):
-        if self.winning_bidder:
-            self.winning_bid_time = datetime.now()
+        if self.winning_bidder_id:
+            self.winning_bid_time = timezone.now()
         super().save(*args, **kwargs)
 
 
 class Bidder(models.Model):
 
-    bidder_num = models.CharField(max_length=8, blank=False)
-    name = models.CharField(max_length=100, blank=False)
+    bidder_num = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
 
     def get_absolute_url(self):
         return reverse('bidder_detail', kwargs={'pk': self.pk})
+
+
