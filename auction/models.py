ef8eda7731faece04d80ad8cdc3b3721133582d8
Paul Ortman <paul.ortman@gmail.com>
2017-09-18 22:48:20 -0500

    add Charge model

--- auction/models.py
+++ auction/models.py
@@ -11,34 +11,33 @@ class AuctionItem(models.Model):
 
     name = models.CharField(max_length=50, blank=False)
     long_desc = models.TextField(blank=True)
-    winning_bidder = models.ForeignKey('Bidder', blank=True, null=True)
     winning_bid_time = models.DateTimeField(blank=True, null=True)
     auction_time = models.DateTimeField(blank=True, null=True)
+    charge = models.ForeignKey('Charge', blank=True, null=True)
 
     def __str__(self):
-        return "({}) {}{}".format(self.id, self.name, "*" if self.winning_bidder_id else "")
+        return "({}) {}{}".format(self.id, self.name, "*" if self.charge else "")
 
     def get_absolute_url(self):
         return reverse('auctionitem_detail', kwargs={'pk': self.pk})
 
     def save(self, *args, **kwargs):
-        if self.winning_bidder_id:
+        if self.charge:
             self.winning_bid_time = timezone.now()
         super().save(*args, **kwargs)
 
 
 @receiver(post_save, sender=AuctionItem)
 def send_sale_event(sender, instance, **kwargs):
-    if instance.winning_bidder_id:
+    if instance.charge_id:
         Group('sales_events').send({
             'text': json.dumps({
                 'id': instance.id,
                 'name': instance.name,
-                'winning_bidder_id': instance.winning_bidder_id
+                'charge_id': instance.charge_id
             })
         })
         print("Sent Event")
-
 
 
 class Bidder(models.Model):
@@ -49,4 +48,26 @@ class Bidder(models.Model):
     def get_absolute_url(self):
         return reverse('bidder_detail', kwargs={'pk': self.pk})
 
+    @property
+    def outstanding_charges_total(self):
+        s = self.charges.filter(state=Charge.UNPAID).aggregate(models.Sum('amount'))['amount__sum']
+        return s if s else 0
+
+
+class Charge(models.Model):
+    UNPAID = 'UNPAID'
+    PAID = 'PAID'
+    VOID = 'VOID'
+    STATES = (
+        (UNPAID, 'Unpaid'),
+        (PAID, 'Paid'),
+        (VOID, 'Void')
+    )
+
+    bidder = models.ForeignKey('Bidder', related_name='charges')
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    state = models.CharField(choices=STATES, default='UNPAID', max_length=7)
+
+    def get_absolute_url(self):
+        return reverse('charge_detail', kwargs={'pk': self.pk})
 
--- auction/tests.py
+++ auction/tests.py
@@ -1,12 +1,16 @@
+from decimal import Decimal
 from django.test import TestCase
 
+from .models import Charge
 from .modelfactory import AuctionItemFactory, BidderFactory
+
 
 class AuctionBidEntry(TestCase):
 
     def test_winning_bid_time_set(self):
         b = BidderFactory()
-        ai = AuctionItemFactory(winning_bidder = b)
+        c = Charge.objects.create(bidder=b, amount='10')
+        ai = AuctionItemFactory(charge=c)
         ai.save()
         assert ai.winning_bid_time is not None
 
@@ -14,3 +18,31 @@ class AuctionBidEntry(TestCase):
         ai = AuctionItemFactory()
         ai.save()
         assert ai.winning_bid_time is None
+
+
+class ChargesTestCase(TestCase):
+
+    def test_no_charges(self):
+        b = BidderFactory()
+        assert b.outstanding_charges_total == 0
+
+    def test_one_charge(self):
+        b = BidderFactory()
+        Charge.objects.create(bidder=b, amount='10.00')
+        assert b.outstanding_charges_total == Decimal('10.00')
+
+    def test_multiple_charges(self):
+        b = BidderFactory()
+        Charge.objects.create(bidder=b, amount='10.00')
+        Charge.objects.create(bidder=b, amount='10.00')
+        Charge.objects.create(bidder=b, amount='10.00')
+        Charge.objects.create(bidder=b, amount='10.00')
+        assert b.outstanding_charges_total == Decimal('40.00')
+
+    def test_some_paid(self):
+        b = BidderFactory()
+        Charge.objects.create(bidder=b, amount='10.00')
+        Charge.objects.create(bidder=b, amount='10.00')
+        Charge.objects.create(bidder=b, amount='10.00', state=Charge.PAID)
+        Charge.objects.create(bidder=b, amount='10.00', state=Charge.PAID)
+        assert b.outstanding_charges_total == Decimal('20.00')
