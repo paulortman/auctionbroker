7c9134fb29b6633c4ce18cbcc421cfdcd2eb0e6a
Paul Ortman <paul.ortman@gmail.com>
2018-02-24 10:18:26 -0600

    wip

--- auction/management/commands/populate_testdb.py
+++ auction/management/commands/populate_testdb.py
@@ -1,7 +1,43 @@
+import random
+
 from django.core.management.base import BaseCommand, CommandError
 
 from auction.modelfactory import BoothFactory, BuyerFactory, ItemFactory
 
+items = [
+    "1945 Massey Harris",
+    "1952 MG Sports Car",
+    "1965 Chevy 60 Farm Truck With Hoist",
+    "1968 F-100 Ford Pickup",
+    "1988 S-10 Chevy Pickup",
+    "1993 Honda Helix",
+    "A-C toy tractor and John Deere metal sign",
+    "Antique Hall Tree",
+    "Antique Oak Buffet",
+    "Bamboo Rocking Horse",
+    "Catered Pork Chop Meal",
+    "Child’s Rocker",
+    "FOUR Meal Deals for up to 25 people",
+    "FarmersEdge Service",
+    "Hand Crafted Adirondack Chairs",
+    "Hustler MDV LeveLift",
+    "L111 John Deere Hydrostatic Mower",
+    "LARGE 36 Inch Life Like Dolls",
+    "Mowers, Blower, Edger & Power Tools",
+    "Mudroom Storage Bench",
+    "Professionally Restored 1950 8N Ford Tractor",
+    "Quilt Rack",
+    "Quilters Diorama/Shadowbox",
+    "Restored 1949 Schwinn Black Phantom Bicycle",
+    "Schwinn Bicycle",
+    "Smart VR",
+    "Vintage 110 John Deere ",
+    "bamboo rocking horse",
+    "mudroom storage bench",
+    "StarBurst Quilt",
+    "Children's Quilt",
+    "Chocolate Heaven"
+]
 
 class Command(BaseCommand):
     help = 'Create a bunch of test stuff in the DB'
@@ -16,6 +52,6 @@ class Command(BaseCommand):
         for i in range(200):
             BuyerFactory.create()
 
-        for i in range(50):
-            ItemFactory.create(booth=auction)
+        for i in random.shuffle(items):
+            ItemFactory.create(booth=auction, name=i)
 
