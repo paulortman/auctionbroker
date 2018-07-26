d22f96876f80cdff04ee85b3d5be212caef26541
Paul Ortman <paul.ortman@gmail.com>
2018-07-26 14:13:50 -0500

    use SSL only for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,3 +14,6 @@ DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=Tru
 MIDDLEWARE = [MIDDLEWARE[0], 'whitenoise.middleware.WhiteNoiseMiddleware', ] + MIDDLEWARE[1:]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Force SSL on Heroku: https://help.heroku.com/J2R1S4T8/can-heroku-force-an-application-to-use-ssl-tls
+SECURE_SSL_REDIRECT = True
