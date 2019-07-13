47301f67209e66df8f0c1849c48ffe103f8f50ad
Paul Ortman <paul.ortman@gmail.com>
2019-07-12 20:43:21 -0500

    whitespace for release

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 ENV = os.environ['ENV']
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
