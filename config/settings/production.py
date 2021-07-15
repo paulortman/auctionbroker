4e1e717922d36f8c8c23cf170d3126c3a25cc059
Paul Ortman <paul.ortman@gmail.com>
2021-07-14 21:33:28 -0500

    update all packages, install Sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from .base import *
 
 
@@ -17,3 +20,19 @@ SECURE_SSL_REDIRECT = True
 import django_heroku
 
 django_heroku.settings(locals())
+
+
+# Sentry
+sentry_sdk.init(
+    dsn="https://206edf948b6a4381bcd295e0be7dda57@o528023.ingest.sentry.io/5866008",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
